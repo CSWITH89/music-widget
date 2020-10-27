7cc6221887cb18851c1ed7f4e5c170d500e5bbcb
Chris Withall <chriswithall@Chriss-MacBook-Pro.local>
2020-10-27 22:57:04 +0000

    API Call fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import { useState, useEffect } from "react";
-import APIController from "./controller/APIController";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState();
+  const [playlists, setPlaylists] = useState([]);
 
-  console.log("api ", APIController.getToken());
+  const AuthenticationId = "M2YwODRlYjk4Njc1NDdjNDk3MTExN2E4NjY0ZTI3N2Q6Y2NhOGQ0NTc4YTZmNGE0YmJmMmExN2MzMmJjYTNmNzk=";
 
   useEffect(() => {
-    const loadToken = async () => {
-      let result = await APIController.getToken();
-      console.log('result ', result);
-      setToken(result);
-    }
 
-    loadToken();
+    const getToken = async () => {
+      let result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+          Authorization: `Basic ${AuthenticationId}`,
+        },
+        body: "grant_type=client_credentials",
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log("response access token ",response.access_token);
+          return response.access_token;
+        })
+        .catch((ex) => {
+          console.error("Error ", ex);
+        });
+
+      console.log("token result ", result);
+
+      setToken(result);
+    };
+
+    getToken();
 
   }, []);
 
   useEffect(() => {
+
+    const getPlaylist = async (playlistId, accessToken) => {
+      let result = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+        method: "GET",
+        headers: {
+          'Authorization': 'Bearer ' + accessToken
+        }
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log('playlist response ', response)
+          return response;
+        })
+        .catch((ex) => {
+          console.error("Error ", ex);
+        });
+
+      let updatedArray = playlists;
+      updatedArray.push(result);
+      setPlaylists(updatedArray);
+      
+        console.log('updatedArray ', playlists);
+    };
+
+
     if (token) {
+      const getAllPlaylists = () => {
+        return Promise.all([
+      getPlaylist('37i9dQZF1DWXRqgorJj26U', token),
+      getPlaylist('37i9dQZF1DWWGFQLoP9qlv', token),
+      getPlaylist('37i9dQZEVXbKCF6dqVpDkS', token)
+          ])
+      }
+  
+      getAllPlaylists();
       setLoading(false);
     }
   }, [token]);
--- src/controller/APIController.js
+++ src/controller/APIController.js
@@ -1,34 +1,131 @@
-const APIController = (() => {
+// const API = () => {
+//   const AuthenticationId =
+//     "M2YwODRlYjk4Njc1NDdjNDk3MTExN2E4NjY0ZTI3N2Q6Y2NhOGQ0NTc4YTZmNGE0YmJmMmExN2MzMmJjYTNmNzk=";
 
-    const AuthenticationId = "M2YwODRlYjk4Njc1NDdjNDk3MTExN2E4NjY0ZTI3N2Q6Y2NhOGQ0NTc4YTZmNGE0YmJmMmExN2MzMmJjYTNmNzk=";
+//   const _getToken = async () => {
+//     await fetch("https://accounts.spotify.com/api/token", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+//         Authorization: `Basic ${AuthenticationId}`,
+//       },
+//       body: "grant_type=client_credentials",
+//     })
+//       .then((response) => response.json())
+//       .then((response) => {
+//         console.log(
+//           "response before return ",
+//           response,
+//           " ",
+//           response.access_token
+//         );
+//         return response.access_token;
+//       })
+//       .catch((ex) => {
+//         console.error("Error ", ex);
+//       });
+//   };
 
-    const _getToken = async () => {
+//   const _getPlaylist = async (token, endpoint) => {
+//     await fetch(`${endpoint}`, {
+//       method: "GET",
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then((response) => {
+//         console.log("playlist ", response);
+//       })
+//       .catch((ex) => {
+//         console.error("Error ", ex);
+//       });
+//   };
 
+//   const _getPlaylistTracks = async (token, endpoint) => {
+//     await fetch(`${endpoint}`, {
+//       method: "GET",
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then((response) => {
+//         console.log("playlist tracks ", response);
+//       })
+//       .catch((ex) => {
+//         console.error("Error ", ex);
+//       });
+//   };
+
+//   return {
+//     getToken() {
+//       return _getToken();
+//     },
+//     getPlaylist(token, endpoint) {
+//       return _getPlaylist(token, endpoint);
+//     },
+//     getPlaylistTracks(token, endpoint) {
+//       return _getPlaylistTracks(token, endpoint);
+//     },
+//   };
+// };
+
+const AuthenticationId =
+    "M2YwODRlYjk4Njc1NDdjNDk3MTExN2E4NjY0ZTI3N2Q6Y2NhOGQ0NTc4YTZmNGE0YmJmMmExN2MzMmJjYTNmNzk=";
+
+export const getToken = async () => {
     await fetch("https://accounts.spotify.com/api/token", {
-    method: "post",
-    headers: {
+      method: "POST",
+      headers: {
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-        "Authorization": `Basic ${AuthenticationId}`
-    },
-    body: "grant_type=client_credentials"
-})
-    .then(response => response.json())
-    .then(response => {
-        console.log('response before return ',response);
+        Authorization: `Basic ${AuthenticationId}`,
+      },
+      body: "grant_type=client_credentials",
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(
+          "response before return ",
+          response,
+          " ",
+          response.access_token
+        );
         return response.access_token;
-    }).catch(ex => {
+      })
+      .catch((ex) => {
         console.error("Error ", ex);
-    });
+      });
+  };
 
-    }
+  export const getPlaylist = async (token, endpoint) => {
+    await fetch(`${endpoint}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("playlist ", response);
+      })
+      .catch((ex) => {
+        console.error("Error ", ex);
+      });
+  };
 
-
-    return {
-        getToken() {
-            console.log('private ', _getToken());
-            return _getToken();
-        },
-    };
-})();
-
-export default APIController;
+  export const getPlaylistTracks = async (token, endpoint) => {
+    await fetch(`${endpoint}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("playlist tracks ", response);
+      })
+      .catch((ex) => {
+        console.error("Error ", ex);
+      });
+  };
