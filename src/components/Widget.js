b397dd8cd8df119924bee4851ef31fd8b64a5227
Chris Withall <chriswithall@chriss-mbp.home>
2020-11-05 11:45:59 +0100

    night mode creation

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -2,6 +2,7 @@ import React,{ useState } from "react";
 import TestImage from "../images/stage.jpg";
 
 const Widget = ({ playlistData }) => {
+  const [nightMode, setNightMode] = useState(false);
   const [currentAlbum, setCurrentAlbum] = useState();
   const [currentTrackcount, setCurrentTrackCount] = useState(null);
   const [AlbumArt, setAlbumArt] = useState(TestImage);
@@ -15,32 +16,38 @@ const Widget = ({ playlistData }) => {
     setAlbumArt(image);
   }
 
-  return (
-    <div className="widget">
-      <section className="widget__left">
-        <div className="widget__left--trackcount">{currentTrackcount}</div>
-        <img alt="cover" className="widget__left--image" src={AlbumArt}></img>
-      </section>
-      <section className="widget__right">
-        <div className="widget__right--toprow">
-            <p className="widget__right--toprow__title">PLAYLISTS</p>
-          <div className="widget__right--toprow__circle">
-            <i class="far fa-moon"></i>
+  if (nightMode) {
+    // Night Mode Component
+  } else {
+    return (
+      <div className="widget">
+        <section className="widget__left">
+          <div className="widget__left--details">{`${currentAlbum}: ${currentTrackcount} Songs`}</div>
+          <img alt="cover" className="widget__left--image" src={AlbumArt}></img>
+        </section>
+        <section className="widget__right">
+          <div className="widget__right--toprow">
+              <p className="widget__right--toprow__title">PLAYLISTS</p>
+            <div onClick={()=>setNightMode(true)} className="widget__right--toprow__circle">
+              <i class="far fa-moon"></i>
+            </div>
           </div>
-        </div>
-        {playlistData.map((playlist) => {
-          return <div 
-          id={playlist.name}
-           className="widget__right--option"
-           onClick={()=>playlistClick(playlist.name, playlist.trackcount, playlist.image)}
-           >{playlist.name}</div>
-        })}
-        {/* <div className="widget__right--option">playlist 1</div>
-        <div className="widget__right--option">playlist 2</div>
-        <div className="widget__right--option">playlist 3</div> */}
-      </section>
-    </div>
-  );
+          {playlistData.map((playlist) => {
+            return <div 
+            id={playlist.name}
+             className="widget__right--option"
+             onClick={()=>playlistClick(playlist.name, playlist.trackcount, playlist.image)}
+             >{playlist.name}</div>
+          })}
+          {/* <div className="widget__right--option">playlist 1</div>
+          <div className="widget__right--option">playlist 2</div>
+          <div className="widget__right--option">playlist 3</div> */}
+        </section>
+      </div>
+    );
+  }
+
+  
 };
 
 export default Widget;
